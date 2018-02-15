d1eea4f8935fbe46f28af1043160cb6543e59c08
devcomm <devcomm50@gmail.com>
2018-02-15 03:20:34 -0800

    scholars pagination

--- app/controllers/search/scholars_controller.rb
+++ app/controllers/search/scholars_controller.rb
@@ -3,17 +3,24 @@ class Search::ScholarsController < ApplicationController
   def index
     @limit = 9
     @links = 2
-    @total = Scholar.count()
+
     @page = params[:page].to_i
+
+    if not params[:s].nil?
+      @where = "full_name like '#{params[:s]}%'"
+    end
+
+    @total = Scholar.where(@where).count()
     @last = (@total / @limit).ceil
     @start = ((@page - @links) > 0) ? @page - @links : 1
     @end = ((@page + @links) < @last) ? @page + @links : @last
 
+
     if params[:page].nil? || params[:page].to_i < 1
-      @scholars = Scholar.limit(@limit)
+      @scholars = Scholar.where(@where).limit(@limit)
       @page = 1
     else
-      @scholars = Scholar.offset((@page - 1) * @limit).limit(@limit)
+      @scholars = Scholar.where(@where).offset((@page - 1) * @limit).limit(@limit)
     end
   end
 
