4057f38ca06e1d8cfcc256b22159dd5cfa4d64bf
devcomm <devcomm50@gmail.com>
2018-02-20 15:55:38 -0800

    update scholars

--- app/controllers/search/scholars_controller.rb
+++ app/controllers/search/scholars_controller.rb
@@ -3,18 +3,17 @@ class Search::ScholarsController < ApplicationController
   def index
     @limit = 9
     @links = 2
-
     @page = params[:page].to_i
 
     if not params[:s].nil?
       @where = "full_name like '#{params[:s]}%'"
+      @query = "&s=#{params[:s]}"
     end
 
     @total = Scholar.where(@where).count()
     @last = (@total / @limit).ceil
     @start = ((@page - @links) > 0) ? @page - @links : 1
     @end = ((@page + @links) < @last) ? @page + @links : @last
-
 
     if params[:page].nil? || params[:page].to_i < 1
       @scholars = Scholar.where(@where).limit(@limit)
