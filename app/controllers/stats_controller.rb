3e4ac6c7b660f263d45467fe3b234e949186e325
devcomm <devcomm50@gmail.com>
2018-02-21 07:32:11 -0800

    update states dropdown

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,16 @@
 class StatsController < ApplicationController
-  SEARCH_TYPES = ["Members", "Scholars"]
+
   def index
 
   end
 
   def explore
+    @country = params[:country]
+    if @country == "usa"
+      @states = States.instance.states[:usa]
+    else
+      @states = States.instance.states[:canada]
+    end
 
   end
 end
