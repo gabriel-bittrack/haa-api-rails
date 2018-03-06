b9bf63d13c517fc2bed053d40c0a7d8b0fbcc78e
Mike O'Dell <mikeodell77@gmail.com>
2018-03-05 23:00:37 -0500

    Adding more debugging

--- app/controllers/search/scholars_controller.rb
+++ app/controllers/search/scholars_controller.rb
@@ -4,5 +4,6 @@ class Search::ScholarsController < ApplicationController
     @scholar_class_years = Scholar.distinct_class_years
     @scholar_states = States.instance.states["usa"]
     puts ">>>>> scholar_class_years : #{@scholar_class_years}"
+    puts ">>>>> scholar states : #{@scholar_states}"
   end
 end
