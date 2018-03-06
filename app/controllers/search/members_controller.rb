d61a019c0543c971f8f6170108d565c59617f40f
Mike O'Dell <mikeodell77@gmail.com>
2018-03-05 20:11:27 -0500

    Adding search code

--- app/controllers/search/members_controller.rb
+++ app/controllers/search/members_controller.rb
@@ -1,30 +1,5 @@
 class Search::MembersController < ApplicationController
-
   def index
-    @limit = 9
-    @links = 2
-    @page = params[:page].to_i
-
-    puts ">>>> What are params for member search : #{params.inspect}"
-    # @country = params[:country] 
-    @country = 'usa'
-    @states = States.instance.states[@country]
-
-    if not params[:s].nil?
-      @where = "full_name like '#{params[:s]}%'"
-      @query = "&s=#{params[:s]}"
-    end
-
-    @total = Member.where(@where).count()
-    @last = (@total / @limit).ceil
-    @start = ((@page - @links) > 0) ? @page - @links : 1
-    @end = ((@page + @links) < @last) ? @page + @links : @last
-
-    if params[:page].nil? || params[:page].to_i < 1
-      @members = Member.where(@where).limit(@limit)
-      @page = 1
-    else
-      @members = Member.where(@where).offset((@page - 1) * @limit).limit(@limit)
-    end
+    @members = Member.search(params, params[:page])
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,12 @@
 require 'csv'
 
 class Member < ApplicationRecord
+  self.per_page = 25
+  scope :year, ->(class_year) { where("class_year = ?", class_year) if class_year.present? }
+  scope :search_name, ->(name) { where("full_name LIKE ?", "%#{name}%") if name.present? }
+  scope :search_state, -> (state) { where("state = ?", state) if state.present? }
+  scope :search_industry, -> (industry) { where("industry LIKE ?", "%#{industry}%") if industry.present? }
+
   has_attached_file :profile_image, style: {
     original: ["100%", :png],
     thumb: ['100x100', :png],
@@ -11,6 +17,30 @@ class Member < ApplicationRecord
   validates_attachment :profile_image,
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   }
+
+  def self.distinct_industries
+    members = Member.select(:industry).distinct
+    industries = []
+
+    members.each do |member|
+      unless member.industry.nil?
+        member_industries = member.industry.split(";")
+        member_industries.each do |industry|
+          industries << industry unless industries.include? industry      
+        end
+      end
+    end
+
+    return industries.sort!
+  end
+
+  def self.search(search, page)
+    Member.year(search[:class_year])
+          .search_name(search[:s])
+          .search_state(search[:state])
+          .search_industry(search[:industry])
+          .page(page)
+  end
 
   def self.to_csv(fields = column_names, options = {})
     CSV.generate(options) do |csv|
