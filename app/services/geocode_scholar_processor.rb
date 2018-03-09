83d551e519a8e1b5fe0127c20736989bd1b9f8c5
Mike O'Dell <mike.odell@salesloft.com>
2018-03-09 10:03:07 -0500

    rerun geocode for remaining scholars and reveal in api

--- app/services/geocode_scholar_processor.rb
+++ app/services/geocode_scholar_processor.rb
@@ -5,7 +5,8 @@ class GeocodeScholarProcessor < SyncProcessor
 
   def process
     puts ">>>> let the processing begin"
-    scholars = Scholar.pluck(:id)
+    # scholars = Scholar.pluck(:id)
+    scholars = Scholar.where('lat is null').pluck(:id)
     puts ">>>> process these ids : #{scholars}"
     ScholarCoordinatesWorker.perform_async(scholars)
   end
